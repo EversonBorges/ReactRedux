c4ada066726d25bfd4afc087ba96ba3f920210df
EversonBorges <everson.cursos@gmail.com>
2021-12-18 18:48:43 -0300

    teste

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Wrapper } from './AppContainer.styles'
 
 function AppContainer ({ left, middle, right }) {
+  
   return <Wrapper>
     <div>{ left }</div>
     <div>{ middle }</div>
